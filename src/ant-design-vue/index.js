d1a6c9f17fe6a308706be77f3939c8e5b40d19e8
lj0812 <xoyljlj@163.com>
2020-08-18 23:09:57 +0800

    add ant component

--- src/ant-design-vue/index.js
+++ src/ant-design-vue/index.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import {
   Button,
   Input,
-  Icon
+  Icon,
+  Steps,
+  Upload
 } from 'ant-design-vue'
 
 Vue.use(Button)
 Vue.use(Input)
 Vue.use(Icon)
+Vue.use(Steps)
+Vue.use(Upload)
